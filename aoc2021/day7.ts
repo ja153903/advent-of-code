e27b33cf3ec415c5fb7590d461c7c73401f2f5d8
Jaime Abbariao <jaimeabbariao@gmail.com>
2021-12-07 19:45:00 -0500

    adding stuff

--- aoc2021/day7.ts
+++ aoc2021/day7.ts
@@ -7,7 +7,6 @@ async function parseFile(): Promise<number[]> {
 
   for await (const line of readLines(file)) {
     result = line.split(',').map(ch => parseInt(ch) ?? -1)
-    break
   }
 
   return result
@@ -15,6 +14,36 @@ async function parseFile(): Promise<number[]> {
 
 const crabs: number[] = await parseFile()
 
+const maxPos = Math.max(...crabs)
+const minPos = Math.min(...crabs)
+
 // Part 1
+let minFuel = Number.MAX_VALUE
+for (let i = minPos; i <= maxPos; i++) {
+  const curFuel = crabs
+      .map(crab => Math.abs(i - crab))
+      .reduce((acc, num) => acc + num)
+
+  minFuel = Math.min(minFuel, curFuel)
+}
+
+console.log(minFuel)
 
 // Part 2
+function getSumUpToN(n: number): number {
+  return n * (n + 1) / 2
+}
+
+minFuel = Number.MAX_VALUE
+for (let i = minPos; i <= maxPos; i++) {
+  const curFuel = crabs
+      .map(crab => {
+        const diff = Math.abs(i - crab)
+        return getSumUpToN(diff)
+      })
+      .reduce((acc, num) => acc + num)
+
+  minFuel = Math.min(minFuel, curFuel)
+}
+
+console.log(minFuel)
