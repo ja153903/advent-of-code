549d1c51b2f0aafe781dceb31fcd979aff9cc9e1
ja153903 <jaimeabbariao@gmail.com>
2024-08-23 01:07:50 -0400

    updating with day 3 2023

--- yr2023/day3/main.ts
+++ yr2023/day3/main.ts
@@ -1,3 +1,5 @@
+import { DIRS_WITH_DIAGONALS } from "../../utils/constants";
+
 async function readData() {
   const filepath = `${import.meta.dir}/data.in`;
   const file = Bun.file(filepath);
@@ -9,74 +11,147 @@ async function readData() {
     .map((line) => line.split(""));
 }
 
-type QueueItem = [number, number];
+type NumberMetadata = {
+  value: number;
+  row: number;
+  start: number;
+  end: number;
+};
 
-function isSymbol(char: string): boolean {
-  if (char >= "0" && char <= "9") {
-    return false;
-  }
+function fetchNumbersPerRow(line: string[], row: number): NumberMetadata[] {
+  const result: NumberMetadata[] = [];
+  let currentNum = "";
+  let start = line.length;
+  let end = 0;
 
-  if (char === ".") {
-    return false;
-  }
-
-  return true;
-}
-
-function enqueueSymbols(data: string[][]): QueueItem[] {
-  const items: QueueItem[] = [];
-
-  for (let i = 0; i < data.length; i += 1) {
-    for (let j = 0; j < data[i].length; j += 1) {
-      if (isSymbol(data[i][j])) {
-        items.push([i, j]);
-      }
+  for (let i = 0; i < line.length; i += 1) {
+    if (line[i] >= "0" && line[i] <= "9") {
+      currentNum += line[i];
+      start = Math.min(start, i);
+      end = Math.max(end, i);
+    } else if (start <= end) {
+      result.push({ value: parseInt(currentNum), row, start, end });
+      currentNum = "";
+      start = line.length;
+      end = 0;
     }
   }
 
-  return items;
-}
-
-const DIRS = [
-  [0, 1],
-  [1, 0],
-  [1, 1],
-  [-1, 0],
-  [0, -1],
-  [-1, -1],
-  [-1, 1],
-  [1, -1],
-];
-
-// TODO: Figure out how should this should be implemented
-function collectPartNumbers(data: string[][], origin: QueueItem): number[] {
-  // for each origin, we should look around to see if there are any numbers
-  // this should be done in a BFS manner so we can easily remove overlapping items
-  const visited = new Set<string>();
-  const partNumbers: number[] = [];
-
-  for (const [dx, dy] of DIRS) {
-    const cx = origin[0] + dx;
-    const cy = origin[1] + dy;
+  // Make sure we add the last value
+  if (start <= end) {
+    result.push({ value: parseInt(currentNum), row, start, end });
   }
 
-  return partNumbers;
+  return result;
+}
+
+function isAdjacentToSymbol(number: NumberMetadata, data: string[][]): boolean {
+  const { row, start, end } = number;
+
+  for (let i = start; i <= end; i += 1) {
+    for (const [a, b] of DIRS_WITH_DIAGONALS) {
+      const newRow = row + a;
+      const newCol = i + b;
+
+      if (
+        newRow < 0 ||
+        newCol < 0 ||
+        newRow >= data.length ||
+        newCol >= data[0].length
+      ) {
+        continue;
+      }
+
+      if (
+        (data[newRow][newCol] >= "0" && data[newRow][newCol] <= "9") ||
+        data[newRow][newCol] === "."
+      ) {
+        continue;
+      }
+
+      return true;
+    }
+  }
+
+  return false;
 }
 
 async function solvePart1() {
   const data = await readData();
-  const origins = enqueueSymbols(data);
-  let result = 0;
-
-  for (const origin of origins) {
-    const partNumbers = collectPartNumbers(data, origin);
-    result += partNumbers.reduce((acc, num) => acc + num, 0);
-  }
+  const result = data
+    .flatMap((line, index) => fetchNumbersPerRow(line, index))
+    .filter((number) => isAdjacentToSymbol(number, data))
+    .reduce((acc, number) => acc + number.value, 0);
 
   console.log(`Year 2023 - Day 3 - Part 1: ${result}`);
 }
 
-function solvePart2() {}
+function isAdjacentToStar(
+  number: NumberMetadata,
+  data: string[][]
+): { isAdjacent: boolean; key: string } {
+  const { row, start, end } = number;
+
+  for (let i = start; i <= end; i += 1) {
+    for (const [a, b] of DIRS_WITH_DIAGONALS) {
+      const newRow = row + a;
+      const newCol = i + b;
+
+      if (
+        newRow < 0 ||
+        newCol < 0 ||
+        newRow >= data.length ||
+        newCol >= data[0].length
+      ) {
+        continue;
+      }
+
+      if (
+        (data[newRow][newCol] >= "0" && data[newRow][newCol] <= "9") ||
+        data[newRow][newCol] === "."
+      ) {
+        continue;
+      }
+
+      if (data[newRow][newCol] === "*") {
+        return { isAdjacent: true, key: `${newRow},${newCol}` };
+      }
+    }
+  }
+
+  return { isAdjacent: false, key: "" };
+}
+
+async function solvePart2() {
+  const data = await readData();
+  const numbers = data.flatMap((line, index) =>
+    fetchNumbersPerRow(line, index)
+  );
+
+  const starMap = new Map<string, number[]>();
+
+  for (let i = 0; i < data.length; i += 1) {
+    for (let j = 0; j < data[i].length; j += 1) {
+      if (data[i][j] === "*") {
+        starMap.set(`${i},${j}`, []);
+      }
+    }
+  }
+
+  for (const number of numbers) {
+    const { isAdjacent, key } = isAdjacentToStar(number, data);
+    if (isAdjacent) {
+      starMap.get(key)?.push(number.value);
+    }
+  }
+
+  const result = [...starMap.values()]
+    .filter((value) => value.length === 2)
+    .map((value) => value[0] * value[1])
+    .reduce((acc, value) => acc + value, 0);
+
+  console.log(`Year 2023 - Day 3 - Part 2: ${result}`);
+}
 
 solvePart1();
 solvePart2();
