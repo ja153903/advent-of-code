208a1158bcd0ad88b7649e5eb0c5ffefa3a6dd84
ja153903 <jaimeabbariao@gmail.com>
2024-08-28 21:58:17 -0400

    adding some boilerplate for problem 5

---
+++ yr2023/day5/main.ts
@@ -0,0 +1,16 @@
+async function readData() {
+  const filepath = `${import.meta.dir}/data.in`;
+  const file = Bun.file(filepath);
+
+  const text = await file.text();
+  return text.split("\n\n").filter((line) => line.length > 0);
+}
+
+const data = await readData();
+
+function solvePart1(data: string[]) { }
+
+function solvePart2(data: string[]) { }
+
+solvePart1(data);
+solvePart2(data);
